6596bb7c3661c2824a65d6f8ddf763b5d75c15a7
Joon An <joonseo.an@outlook.com>
2018-07-03 01:54:40 -0400

    edited port

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-console.log(`starting on ${PORT}`);
+// console.log(`starting on ${PORT}`);
 app.listen(PORT);
 
 module.exports = { app };
