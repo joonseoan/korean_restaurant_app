576c0dd333796ea3764f84edc7ae8d3f7ce19bb4
Joon An <joonseo.an@outlook.com>
2018-07-02 22:39:24 -0400

    changed port

--- index.js
+++ index.js
@@ -195,10 +195,8 @@ if (process.env.NODE_ENV === 'production') {
 
 }
 
-const PORT = process.env.PORT;
-
-if(!module.parent){ app.listen(PORT); };
-
-console.log(`Started on port : ${PORT}`);
+const PORT = process.env.PORT || 3000;
+//console.log(`starting on ${PORT}`);
+app.listen(PORT);
 
 module.exports = { app };
